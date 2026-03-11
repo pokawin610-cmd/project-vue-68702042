93b9b4007d172e3327540760795d9e0b2f1228e6
pokawin610-cmd <pokawin610@gmail.com>
2026-03-11 23:43:44 +0700

    Initial commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class="container">
+        <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+        </div>
+
+        <h1>show contact</h1>
+        <h3>Pokawin lasorn</h3> 
+        <p>Welcome to Vue</p>
+
+    </div>
+
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    h3{
+        color: blueviolet;
+    }
+
+
+</style>
